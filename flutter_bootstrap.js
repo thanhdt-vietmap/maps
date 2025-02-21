e40fb4010b2fc90e6db64c5cd4ccb94a5eecd7ee
thanhdt-vietmap <thanhdt@vietmap.vn>
2025-02-21 16:57:28 +0700

    feat: update vector tile

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "901655798"
+    serviceWorkerVersion: "3496619486"
   }
 });
